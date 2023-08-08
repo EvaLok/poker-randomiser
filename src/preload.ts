880a637b03250691ea5921e974a1d9d566c64144
Eva Beekers <eva.aedea@gmail.com>
2023-08-08 16:50:20 +0200

    working (but ugly) example

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,18 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
-    const replaceText = (selector: string, text: string) => {
-        const element = document.getElementById(selector);
-        if (element) {
-            element.innerText = text;
-        }
-    };
 
-    for (const type of ["chrome", "node", "electron"]) {
-        replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+    const initRandomNumberButton = ( element: HTMLElement ) => {
+        element.innerText = 'hello random number box';
+
+        element.onclick = () => {
+            element.innerText = Math.round(Math.random() * 100).toString();
+        };
     }
+
+
+    initRandomNumberButton(document.getElementById(("random-number-button")));
+
+
+
 });
